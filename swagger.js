f6f7531af1fedecad6d69c1cc808eecade995501
stanleynephi <nul23001@byui.edu>
2025-07-17 20:39:13 +0000

    update swagger.js file

--- swagger.js
+++ swagger.js
@@ -8,7 +8,7 @@ const documentation = {
     description:
       "This is the documentation for query database that contains contact information",
   },
-  host: "localhost:8080",
+  host: "https://webservices-yh66.onrender.com",
 }
 
 const outputFile = "./swagger-output.json"
